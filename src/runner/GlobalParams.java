b67c2e66c385e53b797db308bf6c689bd5e151db
Zina K <zina.kuzmin@gmail.com>
2017-05-06 19:24:48 +0300

    Db schema, base classes

--- src/runner/GlobalParams.java
+++ src/runner/GlobalParams.java
@@ -1,5 +1,68 @@
 package runner;
 
 public class GlobalParams {
-	public static boolean SHOULD_INIT_DB = false;
+	
+	/** The parameter for creating clean Database schema */
+	public static boolean SHOULD_INIT_DB = true;
+
+	/**
+	 * Background image path
+	 */
+	public static String IMG_RUNNER_BACKGROUND = "resources/runnerbck.jpg";
+	
+	
+	/**
+	 * On app start server started automatically if value is true
+	 */
+	public static boolean AUTOMATIC_SERVER_START = false;
+
+
+	// /**
+	// * Path of 3D models
+	// */
+	// public static String MODEL_PLANE =
+	// "resources/models/seymourplane-150-im.dae";
+	//
+	//
+	// /**
+	// * Create DB on app start
+	// */
+	// public static boolean SHOULD_INIT_DB = true;
+	//
+	// /**
+	// * Number of cars in each race
+	// */
+	// public static int NUMBER_OF_CARS_IN_RACE = 5; //Do not change
+	//
+	// /**
+	// * System commision from each bet
+	// */
+	// public static double SYSTEM_COMISSION = 0.05;
+	//
+	// /**
+	// * Condition for considering if race is ready to run
+	// */
+	// public static int NUMBER_OF_CARS_IN_RACE_THAT_HAVE_BET = 3;
+	//
+	// /**
+	// * Initial amount of money for new created user
+	// */
+	// public static double USER_INITIAL_AMOUNT_OF_MONEY = 100.0;
+	//
+	// /**
+	// * Number of songs for races run
+	// */
+	// public static int NUMBER_OF_SONGS = 3;
+	//
+	// /**
+	// * How lond race is in state ready_to_start
+	// */
+	// public static int RACE_START_DELAY_IN_MILLISECONDS = 10_000;
+	//
+	// /**
+	// * Number of active races in client view
+	// */
+	// public static int NUMBER_OF_ACTIVE_RACES = 3;
+
+	
 }
